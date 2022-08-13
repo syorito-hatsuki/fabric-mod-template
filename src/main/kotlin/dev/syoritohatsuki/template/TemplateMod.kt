63ac0667ffb103eda78a04905afaac2842ca7b4d
syorito-hatsuki <enderdron3@gmail.com>
2022-08-13 04:24:32 +0300

    Added item registering system and template item

--- src/main/kotlin/dev/syoritohatsuki/template/TemplateMod.kt
+++ src/main/kotlin/dev/syoritohatsuki/template/TemplateMod.kt
@@ -1,13 +1,17 @@
 package dev.syoritohatsuki.template
 
 import com.mojang.logging.LogUtils
+import dev.syoritohatsuki.template.register.ItemsRegister
 import net.fabricmc.api.ModInitializer
 import org.slf4j.Logger
 
 object TemplateMod : ModInitializer {
+
+    val MOD_ID = "template"
     val LOGGER: Logger = LogUtils.getLogger()
 
     override fun onInitialize() {
         LOGGER.info("${javaClass.simpleName} initialized")
+        ItemsRegister
     }
 }
