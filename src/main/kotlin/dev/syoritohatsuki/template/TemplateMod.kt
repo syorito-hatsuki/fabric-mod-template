f19afe9a3a0f6407127efede389ba6f1673ca392
syorito-hatsuki <enderdron3@gmail.com>
2022-08-13 04:24:32 +0300

    Added block registering system and template block

--- src/main/kotlin/dev/syoritohatsuki/template/TemplateMod.kt
+++ src/main/kotlin/dev/syoritohatsuki/template/TemplateMod.kt
@@ -1,13 +1,19 @@
 package dev.syoritohatsuki.template
 
 import com.mojang.logging.LogUtils
+import dev.syoritohatsuki.template.registry.BlocksRegistry
+import dev.syoritohatsuki.template.registry.ItemsRegistry
 import net.fabricmc.api.ModInitializer
 import org.slf4j.Logger
 
 object TemplateMod : ModInitializer {
+
+    val MOD_ID = "template"
     val LOGGER: Logger = LogUtils.getLogger()
 
     override fun onInitialize() {
+        BlocksRegistry
+        ItemsRegistry
         LOGGER.info("${javaClass.simpleName} initialized")
     }
 }
